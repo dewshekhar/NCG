e7a1c2f0823ce98c9abb7c1a0d526aa0859a74fc
dewshekhar <dewshekhar@gmail.com>
2025-02-27 14:12:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
